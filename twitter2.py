b40d9fb6672a1e6a78061f224a36053b0938957c
RostikRK <91615985+RostikRK@users.noreply.github.com>
2022-02-18 23:16:44 +0200

    Update twitter2.py

--- twitter2.py
+++ twitter2.py
@@ -13,32 +13,16 @@ TWITTER_URL = 'https://api.twitter.com/1.1/friends/list.json'
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
-def get_friends_file():
-    cyc_val = True
-    while cyc_val == True:
-        print('')
-        acct = input('Enter Twitter Account: ')
-        if len(acct) < 1:
-            break
-        url = twurl.augment(TWITTER_URL,
-                            {'screen_name': acct, 'count': '10'})
-        #connection = urllib.request.urlopen(url, context=ctx)
-        #data = connection.read().decode()
-        #print(data[:250])
-        #headers = dict(connection.getheaders())
-        # print headers
-        #print('Remaining', headers['x-rate-limit-remaining'])
-        responce = requests.get(url)
-        data = responce.json()
-        with open("friends.json", "w", encoding='utf-8') as fille:
-            json.dump(data, fille, indent=4)
-        print("Change the user? (Y/N). If no we will start navigation with this user timeline.")
-        ans = input()
-        while True:
-            if ans == "N":
-                cyc_val = False
-                break
-            elif ans == "Y":
-                break
-            else:
-                ans = input("Wrong input. Please try again.(Y/N): ")
+def get_friends_file(acct):
+    url = twurl.augment(TWITTER_URL,
+                        {'screen_name': acct, 'count': '50'})
+    #connection = urllib.request.urlopen(url, context=ctx)
+    #data = connection.read().decode()
+    #print(data[:250])
+    #headers = dict(connection.getheaders())
+    # print headers
+    #print('Remaining', headers['x-rate-limit-remaining'])
+    responce = requests.get(url)
+    data = responce.json()
+    with open("friends.json", "w", encoding='utf-8') as fille:
+        json.dump(data, fille, indent=4)
