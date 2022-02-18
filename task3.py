58ad7bb3177b11a74497d3686aa28e6a77c6ae15
RostikRK <91615985+RostikRK@users.noreply.github.com>
2022-02-18 23:17:44 +0200

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,7 +1,6 @@
 """
 Data parser
 """
-import
 import json
 import twitter2
 import folium
@@ -9,10 +8,26 @@ import pandas
 import csv
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
-from operator import itemgetter
 from geopy.exc import GeocoderUnavailable
+from flask import Flask, redirect, url_for, render_template, request
 
-twitter2.get_friends_file()
+app = Flask(__name__)
+
+@app.route("/", methods = ["POST", "GET"])
+def form():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("map", usr = user))
+    else:
+        return render_template("input_page.html")
+
+@app.route("/<usr>")
+def map(usr):
+    twitter2.get_friends_file(usr)
+    generate_csv()
+    generate_map()
+    return render_template("Great_map.html")
+
 
 def delete_multiple_element(list_object, indices):
     """
@@ -29,62 +44,73 @@ def reading_dict(path):
     """
     Reads the file and generates data
     >>> ":-)" == ":-("
-    Мінус пальці
+    False
     """
     with open(path, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
 
-data = reading_dict("friends.json")
 
-header = ["name", "location", "latitude", "longitude"]
+def generate_csv():
+    """
+    Generates the csv file from json
+    """
+    data = reading_dict("friends.json")
 
-users_lists = []
-for uuser in data["users"]:
-    vval1 = uuser["screen_name"]
-    vval2 = uuser["location"]
-    vval2 = vval2.replace(",", "")
-    users_lists.append([vval1, vval2])
+    header = ["name", "location", "latitude", "longitude"]
 
-for userr in users_lists:
-    try:
-        geolocator = Nominatim(user_agent="GeoFinder")
-        geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
-        location = geolocator.geocode(userr[-1])
-        if location == None:
+    users_lists = []
+    for uuser in data["users"]:
+        vval1 = uuser["screen_name"]
+        vval2 = uuser["location"]
+        vval2 = vval2.replace(",", "")
+        users_lists.append([vval1, vval2])
+
+    for userr in users_lists:
+        try:
+            geolocator = Nominatim(user_agent="GeoFinder")
+            geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+            location = geolocator.geocode(userr[-1])
+            if location == None:
+                del userr[-1]
+            else:
+                tupple = (location.latitude, location.longitude)
+                userr.append(tupple[0])
+                userr.append(tupple[1])
+        except GeocoderUnavailable:
             del userr[-1]
-        else:
-            tupple = (location.latitude, location.longitude)
-            userr.append(tupple[0])
-            userr.append(tupple[1])
-    except GeocoderUnavailable:
-        del userr[-1]
 
-bad_user_ind = []
-for usser in users_lists:
-    if len(usser) != 4:
-        bad_user_ind.append(users_lists.index(usser))
+    bad_user_ind = []
+    for usser in users_lists:
+        if len(usser) != 4:
+            bad_user_ind.append(users_lists.index(usser))
 
 
 
-delete_multiple_element(users_lists, bad_user_ind)
+    delete_multiple_element(users_lists, bad_user_ind)
 
-data = ["ljlfjw", "okwfp", "lkjwlwf", "jwlfj"]
 
-with open('user_new.csv', 'w', encoding='UTF8', newline="") as ffile:
-    writerr = csv.writer(ffile)
-    writerr.writerow(header)
-    writerr.writerows(users_lists)
+    with open('user_new.csv', 'w', encoding='UTF8', newline="") as ffile:
+        writerr = csv.writer(ffile)
+        writerr.writerow(header)
+        writerr.writerows(users_lists)
 
-data = pandas.read_csv("user_new.csv")
-latitudee = data["latitude"]
-longiitude = data["longitude"]
-user_name = data["name"]
-locattion = data["location"]
-mapy = folium.Map(zoom_start=10)
-fg = folium.FeatureGroup(name="Films_map")
-for latit, longi, namme, locattion in zip(latitudee, longiitude, user_name, locattion):
-    fg.add_child(folium.Marker(location=(latit, longi), radius=10, popup=namme + "\n" + locattion, icon=folium.Icon(color="red", icon="info-sign")))
-mapy.add_child(fg)
-mapy.save('Great_map.html')
-print(users_lists)
+def generate_map():
+    """
+    Generates the map from csv
+    """
+    data = pandas.read_csv("user_new.csv")
+    latitudee = data["latitude"]
+    longiitude = data["longitude"]
+    user_name = data["name"]
+    locattion = data["location"]
+    mapy = folium.Map(zoom_start=10)
+    fg = folium.FeatureGroup(name="Films_map")
+    for latit, longi, namme, locattion in zip(latitudee, longiitude, user_name, locattion):
+        fg.add_child(folium.Marker(location=(latit, longi), radius=10, popup=namme + "\n" + locattion, icon=folium.Icon(color="red", icon="info-sign")))
+    mapy.add_child(fg)
+    mapy.save('templates/Great_map.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
