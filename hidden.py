a299fcf451e037298f25ded5deff51a0d240c8a4
RostikRK <91615985+RostikRK@users.noreply.github.com>
2022-02-18 23:06:39 +0200

    Update hidden.py

--- hidden.py
+++ hidden.py
@@ -1,5 +1,5 @@
 def oauth():
-    return {"consumer_key": "0PlKlKMbNYWL9UeVuC6DPxX4B",
-            "consumer_secret": "dpvpCGjA9udpjsPkCVfdxZhmAOLMXFeZcZEVLbH3usRiv8fHfN",
-            "token_key":  "963011359737774080-D73fzqJ2jocOftSRejq7tJz2gn4Xa2n",
-            "token_secret": "KJ9zSSyrJsYLidt7PQTyPeAqZvrB92bxWwlzv7Ma8hMcG"}
+    return {"consumer_key": "p6SvBa5BkajbEA7g3NHAQRqi6",
+            "consumer_secret": "pwX3lOC7KewjnPNxBYG0AfkHW24qhdQmTAja0PFzpeyf1jXWZj",
+            "token_key":  "1493226390002491393-q78oSZ1fRoUtqHqDW3gWuyQcj7sR1r",
+            "token_secret": "YFewC106DmmeTVxf2kiqYKz96c62Vm3dqUT3k5lurTymQ"}
